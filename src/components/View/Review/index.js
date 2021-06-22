30a2d4fabc080b80180731b825c1b0f2abc0dd26
vitaliy <vitalya@eyespy360.com>
2021-06-22 17:46:49 +0300

    review bug

--- src/components/View/Review/index.js
+++ src/components/View/Review/index.js
@@ -22,7 +22,7 @@ export default function Index({ documentId, onCallback, reviews }) {
         <Form  onCallback={onAddReview}/>
       </Grid>
       <Grid item xs={12}>
-        {reviews.length &&  <List documentId={documentId} onCallback={onAddReviews} reviews={reviews}/>}
+        {reviews.length ? <List documentId={documentId} onCallback={onAddReviews} reviews={reviews}/> : ''}
       </Grid>
     </Grid>
   )
